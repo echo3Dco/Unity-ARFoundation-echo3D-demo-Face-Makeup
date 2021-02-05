0744292c9f18fe5dbfe1a1a2d839c1bb1cf94a84
bpedazur <30467510+bpedazur@users.noreply.github.com>
2021-02-05 12:22:17 -0500

    first commit

---
+++ CustomBehaviour.cs
@@ -0,0 +1,69 @@
+﻿/**************************************************************************
+* Copyright (C) echoAR, Inc. 2018-2020.                                   *
+* echoAR, Inc. proprietary and confidential.                              *
+*                                                                         *
+* Use subject to the terms of the Terms of Service available at           *
+* https://www.echoar.xyz/terms, or another agreement                      *
+* between echoAR, Inc. and you, your company or other organization.       *
+***************************************************************************/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class CustomBehaviour : MonoBehaviour
+{
+    [HideInInspector]
+    public Entry entry;
+
+    /// <summary>
+    /// EXAMPLE BEHAVIOUR
+    /// Queries the database and names the object based on the result.
+    /// </summary>
+
+    // Use this for initialization
+    void Start()
+    {
+        // Add RemoteTransformations script to object and set its entry
+        this.gameObject.AddComponent<RemoteTransformations>().entry = entry;
+
+        // Qurey additional data to get the name
+        string value = "";
+        if (entry.getAdditionalData() != null && entry.getAdditionalData().TryGetValue("name", out value))
+        {
+            // Set name
+            this.gameObject.name = value;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Set image holgram as face texture
+        Hologram hologram = entry.getHologram();
+        Hologram.hologramType hologramType = hologram.getType();
+        // Check for image hologram
+        if (hologramType == Hologram.hologramType.IMAGE_HOLOGRAM)
+        {
+            // Get mesh renderer
+            MeshRenderer meshRenderer = this.GetComponent<MeshRenderer>();
+            // Get material and texture
+            Material material = meshRenderer.material;
+            Texture texture = material.mainTexture;
+            // Modify shader
+            Shader shader = Shader.Find("Transparent/Diffuse");
+            material.shader = shader;
+            // Get session and face switcher
+            GameObject session = GameObject.Find("AR Session Origin");
+            FaceMaterialSwitcher faceMaterialSwitcher = session.GetComponentInChildren<FaceMaterialSwitcher>();
+            // Set material and texture
+            if (faceMaterialSwitcher != null)
+            {
+                // Set materials (overwrite all other face materials)
+                int size = faceMaterialSwitcher.faceMaterials.Length;
+                faceMaterialSwitcher.faceMaterials = new Material[size];
+                for (int i = 0; i < size; i++) faceMaterialSwitcher.faceMaterials[i] = material;
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,37 @@
+# Unity-ARFoundation-echoAR-Face-Makeup
+
+Simple face makeup demo with Unity, AR Foundation, and echoAR
+
+## Register
+
+Don't have an API key? Make sure to register for FREE at [echoAR](https://console.echoar.xyz/#/auth/register).
+
+## Setup
+
+- Create a new Unity project.
+- Clone the [Unity-ARFoundation-echoAR](https://github.com/echoARxyz/Unity-ARFoundation-echoAR) sample code.
+- Open the sample scene under `AR Foundation\Scenes\FaceTracking\FaceMesh.unity`.
+- [Set your API key](https://docs.echoar.xyz/unity/using-the-sdk) in `echoAR.cs` inside the `echoAR\echoAR.prefab` using the the inspector.
+- Select `AR Session Origin` in the hierarchy. Double click `FaceMeshPrefab` under `AR Face Manager` in the inspector. Set `Material Size` to 1
+- [Add an image hologram](https://docs.echoar.xyz/web-console/manage-pages/content-page/how-to-add-content) by uploding the *[makeup.png](https://github.com/VivianVKJ/Unity-ARFoundation-echoAR-Face-Makeup/blob/main/images/makeup.png)* file from the [images](https://github.com/VivianVKJ/Unity-ARFoundation-echoAR-Face-Makeup/tree/main/images) folder to the echoAR console.
+- Overwrite the existing *echoAR/CustomBehaviour.cs* script with the new [*CustomBehaviour.cs*](https://github.com/VivianVKJ/Unity-ARFoundation-echoAR-Face-Makeup/blob/main/CustomBehaviour.cs) file.
+
+## Create Makeup Texture
+
+Make sure to use an **Alpha transparent texture** as a custom shader. The example texture contains eye shadows, eye liners, hightlight, lipstick or other coloring. When running the app, it detects a face and overlays the face material. 
+
+To create a makeup texture on your own, open the *[.psd](https://github.com/VivianVKJ/Unity-ARFoundation-echoAR-Face-Makeup/blob/main/model/canonical_face_texture.psd)* file in the [model](https://github.com/VivianVKJ/Unity-ARFoundation-echoAR-Face-Makeup/blob/main/model) folder. This model is provided by Google [ARCore ArgumentFaces Demo](https://developers.google.com/ar/develop/developer-guides/creating-assets-for-augmented-faces)
+
+<img src="https://github.com/VivianVKJ/Unity-ARFoundation-echoAR-Face-Makeup/blob/main/demo/model.png" height=40% width=40%>
+
+## Learn more
+
+Refer to our [documentation](https://docs.echoar.xyz/unity/) to learn more about how to use Unity and echoAR.
+
+## Support
+
+Feel free to reach out at [support@echoAR.xyz](mailto:support@echoAR.xyz) or join our [support channel on Slack](https://join.slack.com/t/echoar/shared_invite/enQtNTg4NjI5NjM3OTc1LWU1M2M2MTNlNTM3NGY1YTUxYmY3ZDNjNTc3YjA5M2QyNGZiOTgzMjVmZWZmZmFjNGJjYTcxZjhhNzk3YjNhNjE).
+
+## Screenshot
+
+<img src="https://github.com/VivianVKJ/Unity-ARFoundation-echoAR-Face-Makeup/blob/main/demo/record.gif" height=30% width=30%>
